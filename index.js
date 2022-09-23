660f8e807318dbfcae34c7bfa8e66dd30f5099da
Erik Russell <erikrussells@gmail.com>
2022-09-22 20:45:05 -0400

    commit tree 5500b6c4ec06cffc01cd482a9bd9fd664a1181f6

---
+++ index.js
@@ -0,0 +1,28 @@
+// const divide = (a,b) => {a / b;
+//     return a / b;
+// }
+// divide(2000, 100);
+
+
+
+// const divide = (num1, num2) => {num1 / num2;
+//     return (num1 / num2)
+// }
+// divide(2000,100)
+
+// const square = num3 => num3 * num3;
+
+// const add = (num4, num5) => num4 + num5;
+
+
+
+
+const divide = (x, y) => {
+    return(x / y);
+}
+
+const square = a => a*a;
+
+const add = (c, d) => {
+    return (c+d);
+}
